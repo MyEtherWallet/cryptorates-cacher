c57e923072d761bd33951cb688108976c485cb93
steveMieskoski <stevemieskoski@gmail.com>
2018-07-16 19:02:21 -0700

    add brief syntax notes to README

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get('/meta', function (req, res) {
 })
 
 getPort({
-  port: 41543 // process.env.PORT || 8080
+  port: process.env.PORT || 8080
 }).then(port => {
   var server = app.listen(port, function () {
     var host = server.address().address
