00861a743079152b5c69d7a48cfefa46cf55941f
gamalielhere <gamalielgad1234@gmail.com>
2018-11-30 14:19:37 -0800

    Only load env on development

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if(process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
 const express = require("express");
 const getPort = require("get-port");
 const fetch = require("node-fetch");
